b5de85eb2dea2995ff997bc67ae54fbd8a408b3d
RobynIsBlue <tyglivermont@gmail.com>
2024-11-21 23:12:16 -0600

    KILL ME

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def main():
 	path = "books/frankenstein.txt"
-	print(get_book_text(path))
+	print("--- Begin report of " + str(path) + "---\n")
 	print("Your Word Count is: " + str(word_count(path)))
-	print(character_count(path))
-	print(f"\n The \'{letter}\' character was found {number} times.\n")
+	for x in character_count(path):
+		for y in x:
+			print(f"\n The \'{y}\' character was found {x[y]} times.")
 
 def get_book_text(path):
 	with open(path) as f:
@@ -22,13 +23,18 @@ def character_count(path):
 	with open(path) as f:
 		book = f.read().lower()
 		for x in book:
-			if x in dict:
+			if x.isalpha() == False:
+				None
+			elif x in dict:
 				dict[x] += 1
 			else:
 				dict[x] = 1
 	listed = [{k: v} for k, v in dict.items()]
-	listed.sort(key=sort_on)
-	while len(listed):
+	sortess = sorted[listed, reverse=True]
+	return sortess
+
+def something_idk(big_dic):
+	
 
 main()
 
